43569756c39fae437f0f93382882791ecb0b0bf3
SeyTi01 <jannis.ridder@gmail.com>
2023-09-12 19:47:10 +0200

    added function removeUncommentedActivity, filters uncommented and unliked activity

--- src/hideUncommentedActivity.user.js
+++ src/hideUncommentedActivity.user.js
@@ -4,8 +4,39 @@
 // @version      1.0
 // @description  Hides uncommented activity on home- or social feed
 // @author       SeyTi01
-// @match        https://www.anilist.co/*
+// @match        https://anilist.co/*
 // @grant        none
 // @license      MIT
 // ==/UserScript==
 
+(function() {
+    'use strict';
+
+    function removeUncommentedActivity() {
+        const activityContainer = document.querySelector('.activity-feed');
+
+        if (activityContainer) {
+            const activityObserver = new MutationObserver(function(mutations) {
+                mutations.forEach(function(mutation) {
+                    mutation.addedNodes.forEach(function(node) {
+                        if (node instanceof HTMLElement) {
+                            if (node.classList.contains('activity-entry') && node.classList.contains('activity-anime_list')) {
+                                const countSpan = node.querySelector('span.count');
+                                if (!countSpan) {
+                                    node.remove();
+                                }
+                            }
+                        }
+                    });
+                });
+            });
+
+            const observerConfig = { childList: true, subtree: true };
+            activityObserver.observe(activityContainer, observerConfig);
+        }
+    }
+
+    window.addEventListener('load', function() {
+        removeUncommentedActivity();
+    });
+})();
