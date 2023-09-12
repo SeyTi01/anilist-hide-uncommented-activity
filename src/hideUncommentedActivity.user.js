90ac08598acc86032a3c9a993fabeb7a854c6302
SeyTi01 <jannis.ridder@gmail.com>
2023-09-12 18:33:02 +0200

    comment headlines

---
+++ src/hideUncommentedActivity.user.js
@@ -0,0 +1,11 @@
+// ==UserScript==
+// @name         Anilist: Hide Uncommented Activity
+// @namespace    https://github.com/SeyTi01/
+// @version      1.0
+// @description  Hides uncommented activity on home- or social feed
+// @author       SeyTi01
+// @match        https://www.anilist.co/*
+// @grant        none
+// @license      MIT
+// ==/UserScript==
+
