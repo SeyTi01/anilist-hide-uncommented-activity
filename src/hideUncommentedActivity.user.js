5f5bd62654b4205822440086184cbaaddf84cf77
SeyTi01 <jannis.ridder@gmail.com>
2023-09-13 09:43:48 +0200

    refactor

--- src/hideUncommentedActivity.user.js
+++ src/hideUncommentedActivity.user.js
@@ -1,5 +1,5 @@
 // ==UserScript==
-// @name         (Test) Anilist: Hide Uncommented Activity
+// @name         Anilist: Hide Uncommented Activity
 // @namespace    https://github.com/SeyTi01/
 // @version      1.0
 // @description  Hides uncommented activity on home- or social feed
@@ -16,13 +16,11 @@
     const observer = new MutationObserver(observeMutations);
 
     function removeUncommentedActivity(node) {
-        if (node.nodeType === 1) {
-            if (node.classList.contains('activity-entry') && node.classList.contains('activity-anime_list')) {
-                const countSpan = node.querySelector('span.count');
-                const actionRepliesDiv = node.querySelector('div.action.replies');
-                if (!countSpan || (actionRepliesDiv && !actionRepliesDiv.contains(countSpan))) {
-                    node.remove();
-                }
+        if (node.nodeType === 1 && node.classList.contains('activity-entry') && node.classList.contains('activity-anime_list')) {
+            const countSpan = node.querySelector('span.count');
+            const actionRepliesDiv = node.querySelector('div.action.replies');
+            if (!countSpan || (actionRepliesDiv && !actionRepliesDiv.contains(countSpan))) {
+                node.remove();
             }
         }
     }
@@ -36,9 +34,6 @@
     }
 
     const activityFeed = document.querySelector('.activity-feed');
-    if (activityFeed) {
-        activityFeed.forEach(removeUncommentedActivity);
-    }
-
+    if (activityFeed) {activityFeed.forEach(removeUncommentedActivity);}
     observer.observe(document.body, {childList: true, subtree: true});
 })();
