6a020a81449ad2b5a15997a68260d689159dbf09
SeyTi01 <jannis.ridder@gmail.com>
2023-09-13 16:01:13 +0200

    [FEATURE] automatically load more entries

--- src/hideUncommentedActivity.user.js
+++ src/hideUncommentedActivity.user.js
@@ -1,49 +1,81 @@
-// ==UserScript==
-// @name         Anilist: Hide Uncommented Activity
-// @namespace    https://github.com/SeyTi01/
-// @version      1.0
-// @description  Hides uncommented activity on home- or social feed
-// @author       SeyTi01
-// @match        https://anilist.co/*
-// @grant        none
-// @run-at       document-end
-// @license      MIT
-// ==/UserScript==
+    // ==UserScript==
+    // @name         Anilist: Hide Uncommented Activity
+    // @namespace    https://github.com/SeyTi01/
+    // @version      1.0
+    // @description  Hides uncommented activity on home- or social feed
+    // @author       SeyTi01
+    // @match        https://anilist.co/*
+    // @grant        none
+    // @run-at       document-start
+    // @license      MIT
+    // ==/UserScript==
 
-(function () {
-    'use strict';
+    (function () {
+        'use strict';
 
-    const config = {
-        removeUncommented: true,
-        removeUnliked: false,
-    };
+        const config = {
+            removeUncommented: true,
+            removeUnliked: false,
+            targetLoadCount: 3
+        };
 
-    const activityFeed = document.querySelector('.activity-feed');
-    const observer = new MutationObserver(observeMutations);
+        const observer = new MutationObserver(observeMutations);
+        let currentLoadCount = 0;
+        let userPressedButton = true;
+        let loadMoreButton;
 
-    function removeEntry(node) {
-        if (node.nodeType === 1 && node.classList.contains('activity-entry') && node.classList.contains('activity-anime_list')) {
-            const repliesDiv = node.querySelector('div.action.replies');
-            const likesDiv = node.querySelector('div.action.likes');
+        function removeEntry(node) {
+            let removed = false;
+            if (node.nodeType === 1 && node.classList.contains('activity-entry')) {
+                const repliesDiv = node.querySelector('div.action.replies');
+                const likesDiv = node.querySelector('div.action.likes');
 
-            if (config.removeUncommented && !repliesDiv?.querySelector('span.count')) {
-                node.remove();
+                if (config.removeUncommented && !hasCount(repliesDiv)) {
+                    node.remove();
+                    removed = true;
+                }
+
+                if (config.removeUnliked && !hasCount(likesDiv)) {
+                    node.remove();
+                    removed = true;
+                }
             }
 
-            if (config.removeUnliked && !likesDiv?.querySelector('span.count')) {
-                node.remove();
+            return removed;
+        }
+
+        function observeMutations(mutations) {
+            for (const mutation of mutations) {
+                if (mutation.addedNodes.length !== 0) {
+                    mutation.addedNodes.forEach((node) => {
+                        if (node.nodeType === 1 && node.classList.contains('activity-entry')) {
+                            if (removeEntry(node) === false) {
+                                currentLoadCount++
+                            }
+                        }
+
+                        if (node.nodeType === 1 && node.classList.contains('load-more')) {
+                            loadMoreButton = node;
+                        }
+                    });
+                }
+            }
+
+            if (currentLoadCount < config.targetLoadCount && userPressedButton) {
+                if (loadMoreButton) {
+                    loadMoreButton.click();
+                    loadMoreButton = null;
+                }
+            } else {
+                currentLoadCount = 0;
+                userPressedButton = false;
             }
         }
-    }
 
-    function observeMutations(mutations) {
-        for (const mutation of mutations) {
-            mutation.addedNodes.forEach((node) => {
-                removeEntry(node);
-            });
+        function hasCount(div) {
+            return div?.querySelector('span.count');
         }
-    }
 
-    if (activityFeed) {activityFeed.childNodes.forEach(removeEntry);}
-    observer.observe(document.body, {childList: true, subtree: true});
-})();
+        observer.observe(document.body, {childList: true, subtree: true});
+        loadMoreButton.addEventListener("click", userPressedButton = true)
+    })();
