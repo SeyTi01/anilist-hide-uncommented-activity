cb1ad4fe018fbc764fe1506ec3046565dcaa96d4
SeyTi01 <jannis.ridder@gmail.com>
2023-09-13 18:57:19 +0200

    [REFACTOR] seperated logic into small functions, extracted more selectors, commented config

--- src/hideUncommentedActivity.user.js
+++ src/hideUncommentedActivity.user.js
@@ -14,32 +14,36 @@
     'use strict';
 
     const config = {
-        removeUncommented: false,
-        removeUnliked: true,
-        targetLoadCount: 2
+        removeUncommented: false, // Remove activities with no comments
+        removeUnliked: true, // Remove activities with no likes
+        targetLoadCount: 2 // Minimum number of activities to show per load
     };
 
     const ACTIVITY_SELECTOR = 'activity-entry';
     const BUTTON_SELECTOR = 'load-more';
+    const REPLIES_SELECTOR = 'div.action.replies';
+    const LIKES_SELECTOR = 'div.action.likes';
 
     const observer = new MutationObserver(observeMutations);
     let currentLoadCount = 0;
     let userPressedButton = true;
     let loadMoreButton;
 
-    function removeEntry(node) {
+    observer.observe(document.body, {childList: true, subtree: true});
+
+    function removeEntry(element) {
         let removed = false;
-        if (node instanceof HTMLElement && node.classList.contains(ACTIVITY_SELECTOR)) {
-            const repliesDiv = node.querySelector('div.action.replies');
-            const likesDiv = node.querySelector('div.action.likes');
+        if (element instanceof HTMLElement && element.classList.contains(ACTIVITY_SELECTOR)) {
+            const repliesDiv = element.querySelector(REPLIES_SELECTOR);
+            const likesDiv = element.querySelector(LIKES_SELECTOR);
 
             if (config.removeUncommented && !hasCount(repliesDiv)) {
-                node.remove();
+                element.remove();
                 removed = true;
             }
 
             if (config.removeUnliked && !hasCount(likesDiv)) {
-                node.remove();
+                element.remove();
                 removed = true;
             }
         }
@@ -50,37 +54,45 @@
     function observeMutations(mutations) {
         for (const mutation of mutations) {
             if (mutation.addedNodes.length !== 0) {
-                mutation.addedNodes.forEach((node) => {
-                    if (node instanceof HTMLElement && node.classList.contains(ACTIVITY_SELECTOR)) {
-                        if (removeEntry(node) === false) {
-                            currentLoadCount++
-                        }
-                    }
-
-                    if (node instanceof HTMLElement && node.classList.contains(BUTTON_SELECTOR)) {
-                        loadMoreButton = node;
-                        loadMoreButton.addEventListener('click', function() {
-                            userPressedButton = true;
-                        });
-                    }
-                });
+                mutation.addedNodes.forEach(handleAddedNode);
             }
         }
 
         if (currentLoadCount < config.targetLoadCount && userPressedButton) {
-            if (loadMoreButton) {
-                loadMoreButton.click();
-                loadMoreButton = null;
-            }
+            clickLoadMoreButton();
         } else {
-            currentLoadCount = 0;
-            userPressedButton = false;
+            resetState();
         }
     }
 
-    function hasCount(div) {
-        return div?.querySelector('span.count');
+    function handleAddedNode(node) {
+        if (node instanceof HTMLElement && node.classList.contains(ACTIVITY_SELECTOR)) {
+            if (removeEntry(node) === false) {
+                currentLoadCount++;
+            }
+        }
+
+        if (node instanceof HTMLElement && node.classList.contains(BUTTON_SELECTOR)) {
+            loadMoreButton = node;
+            loadMoreButton.addEventListener('click', function() {
+                userPressedButton = true;
+            });
+        }
     }
 
-    observer.observe(document.body, {childList: true, subtree: true});
+    function hasCount(element) {
+        return element?.querySelector('span.count');
+    }
+
+    function clickLoadMoreButton() {
+        if (loadMoreButton) {
+            loadMoreButton.click();
+            loadMoreButton = null;
+        }
+    }
+
+    function resetState() {
+        currentLoadCount = 0;
+        userPressedButton = false;
+    }
 })();
