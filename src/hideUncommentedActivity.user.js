b0c612418f47e79a700d215e1209793902d76a1a
SeyTi01 <144541737+SeyTi01@users.noreply.github.com>
2023-09-16 18:41:20 +0200

    [RELEASE] 1.4 (#9)
    * [REFACTOR] removed @run-at
    
    * updated README.md
    
    * Update README.md
    
    * [BETA] 1.0 (#3) (#4)
    
    * [REFACTOR] removed @run-at
    
    * updated README.md
    
    * Update README.md
    
    * updated description
    
    * [BUGFIX] changed match url to also apply to manga
    
    * updated version
    
    * [FEATURE] automatically simulate DOM events when site stops loading elements.
    
    * [REFACTOR] version change
    
    * [REFACTOR] refactored/renamed triggerDomEvents for clarity
    
    * [REFACTOR] changed match url to work anywhere on the site
    
    * [FEATURE] added cancel button
    
    * [BUGFIX] multiple cancel buttons created, button not removed when targetLoadCount reached
    
    * [FEATURE] button styling
    
    * [FEATURE] button styling
    
    * [REFACTOR] version change
    
    * [REFACTOR] createCancelButton
    
    * [REFACTOR] updated description
    
    * Observer (#8)
    
    * more specific observer function
    
    * Revert "more specific observer function"
    
    This reverts commit 9bf10abb84a7dd18f5aa0fb843fccc403aa7b8af.
    
    * [REFACTOR] observerMutation, handleAddedNode
    
    * [REFACTOR] removeEntry
    
    * [REFACTOR] removeEntry
    
    * [REFACTOR] SELECTORS, hasCount
    
    * [REFACTOR] observeMutations
    
    * [REFACTOR] createCancelButton
    
    * [REFACTOR] showCancelButton
    
    * [FEATURE] added validateConfig
    
    * [REFACTOR] extracted createCancelButton
    
    * [REFACTOR] function order
    
    * [REFACTOR] function order
    
    * [REFACTOR] handle addedNote, extracted handleLoadMoreButton
    
    * [REFACTOR] changed description
    
    * [REFACTOR] redundant initialisation value
    
    * [REFACTOR]
    
    * updated README.md
    
    * [REFACTOR] version change

--- src/hideUncommentedActivity.user.js
+++ src/hideUncommentedActivity.user.js
@@ -1,8 +1,8 @@
 // ==UserScript==
 // @name         Anilist: Hide Uncommented Activity
 // @namespace    https://github.com/SeyTi01/
-// @version      1.3
-// @description  Hides uncommented/unliked activity on any Anilist activity-feed
+// @version      1.4
+// @description  Hides uncommented/unliked activity on Anilist's activity feeds
 // @author       SeyTi01
 // @match        https://anilist.co/*
 // @grant        none
@@ -19,11 +19,10 @@
     };
 
     const SELECTORS = {
-        activity: 'activity-entry',
-        button: 'load-more',
+        button: 'div.load-more',
+        activity: 'div.activity-entry',
         replies: 'div.action.replies',
         likes: 'div.action.likes',
-        cancel: 'cancel-load'
     };
 
     const observer = new MutationObserver(observeMutations);
@@ -32,26 +31,12 @@
     let loadMoreButton;
     let cancelButton;
 
+    validateConfig(config);
     observer.observe(document.body, {childList: true, subtree: true});
-
-    function removeEntry(element) {
-        let removed = false;
-        if (element instanceof HTMLElement && element.classList.contains(SELECTORS.activity)) {
-            const repliesDiv = element.querySelector(SELECTORS.replies);
-            const likesDiv = element.querySelector(SELECTORS.likes);
-
-            if ((config.removeUncommented && !hasCount(repliesDiv)) || (config.removeUnliked && !hasCount(likesDiv))) {
-                element.remove();
-                removed = true;
-            }
-        }
-
-        return removed;
-    }
 
     function observeMutations(mutations) {
         for (const mutation of mutations) {
-            if (mutation.addedNodes.length !== 0) {
+            if (mutation.addedNodes.length > 0) {
                 mutation.addedNodes.forEach(handleAddedNode);
             }
         }
@@ -64,21 +49,52 @@
     }
 
     function handleAddedNode(node) {
-        if (node instanceof HTMLElement) {
-            if (node.classList.contains(SELECTORS.activity)) {
-                if (!removeEntry(node)) {
-                    currentLoadCount++;
-                }
-
-            } else if (node.classList.contains(SELECTORS.button)) {
-                loadMoreButton = node;
-                loadMoreButton.addEventListener('click', function() {
-                    userPressedButton = true;
-                    simulateDomEvents();
-                    createCancelButton();
-                });
-            }
+        if (!(node instanceof HTMLElement)) {
+            return;
         }
+
+        if (node.matches(SELECTORS.activity)) {
+            if (!removeEntry(node)) {
+                currentLoadCount++;
+            }
+
+        } else if (node.matches(SELECTORS.button)) {
+            handleLoadMoreButton(node);
+        }
+    }
+
+    function removeEntry(node) {
+        let removed = false;
+        const repliesDiv = node.querySelector(SELECTORS.replies);
+        const likesDiv = node.querySelector(SELECTORS.likes);
+
+        if ((config.removeUncommented && !hasCountSpan(repliesDiv)) || (config.removeUnliked && !hasCountSpan(likesDiv))) {
+            node.remove();
+            removed = true;
+        }
+
+        return removed;
+    }
+
+    function handleLoadMoreButton(button) {
+        loadMoreButton = button;
+        loadMoreButton.addEventListener('click', function() {
+            userPressedButton = true;
+            simulateDomEvents();
+            showCancelButton();
+        });
+    }
+
+    function showCancelButton() {
+        if (!cancelButton) {
+            createCancelButton();
+        } else {
+            cancelButton.style.display = 'block';
+        }
+    }
+
+    function hasCountSpan(node) {
+        return node?.querySelector('span.count');
     }
 
     function simulateDomEvents() {
@@ -92,10 +108,6 @@
         }, 100);
     }
 
-    function hasCount(element) {
-        return element?.querySelector('span.count');
-    }
-
     function clickLoadMoreButton() {
         if (loadMoreButton) {
             loadMoreButton.click();
@@ -106,38 +118,45 @@
     function resetState() {
         currentLoadCount = 0;
         userPressedButton = false;
-        if (cancelButton) {
-            cancelButton.remove();
-            cancelButton = null;
-        }
+        cancelButton.style.display = 'none';
     }
 
     function createCancelButton() {
-        if (!cancelButton) {
-            const buttonStyles = `
-            position: fixed;
-            bottom: 10px;
-            right: 10px;
-            z-index: 9999;
-            line-height: 1.3;
-            background-color: rgb(var(--color-background-blue-dark));
-            color: rgb(var(--color-text-bright));
-            font-family: 'Roboto', -apple-system, BlinkMacSystemFont, 'Segoe UI', Oxygen, Ubuntu, Cantarell, 'Fira Sans', 'Droid Sans', 'Helvetica Neue', sans-serif;
-            font-size: 1.6rem;
-            -webkit-font-smoothing: antialiased;
-            box-sizing: border-box;
-        `;
-            cancelButton = document.createElement('button');
-            cancelButton.textContent = 'Cancel';
-            cancelButton.classList.add('cancel-button');
-            cancelButton.style.cssText = buttonStyles;
-            cancelButton.style.setProperty('--button-color', 'rgb(var(--color-blue))');
-            cancelButton.addEventListener('click', function() {
-                userPressedButton = false;
-                cancelButton.remove();
-            });
+        const buttonStyles = `
+                position: fixed;
+                bottom: 10px;
+                right: 10px;
+                z-index: 9999;
+                line-height: 1.3;
+                background-color: rgb(var(--color-background-blue-dark));
+                color: rgb(var(--color-text-bright));
+                font: 1.6rem 'Roboto', -apple-system, BlinkMacSystemFont, 'Segoe UI', Oxygen, Ubuntu, Cantarell, 'Fira Sans', 'Droid Sans', 'Helvetica Neue', sans-serif;
+                -webkit-font-smoothing: antialiased;
+                box-sizing: border-box;
+            `;
 
-            document.body.appendChild(cancelButton);
+        cancelButton = Object.assign(document.createElement('button'), {
+            textContent: 'Cancel',
+            className: 'cancel-button',
+            style: `--button-color: rgb(var(--color-blue)); ${buttonStyles}`,
+            onclick: () => {
+                userPressedButton = false;
+                cancelButton.style.display = 'none';
+            },
+        });
+
+        document.body.appendChild(cancelButton);
+    }
+
+    function validateConfig(config) {
+        const errors = [
+            typeof config.removeUncommented !== 'boolean' && 'removeUncommented must be a boolean',
+            typeof config.removeUnliked !== 'boolean' && 'removeUnliked must be a boolean',
+            (!Number.isInteger(config.targetLoadCount) || config.targetLoadCount < 1) && 'targetLoadCount must be a positive non-zero integer'
+        ].filter(Boolean);
+
+        if (errors.length > 0) {
+            throw new Error(errors.join('\n'));
         }
     }
 })();
