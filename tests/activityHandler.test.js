6a1fd19a5796b844d1875a800d2b530c520e54eb
SeyTi01 <jannis.ridder@gmail.com>
2023-10-09 13:28:08 +0200

    tests for uncommented, images, videos + test data

--- tests/activityHandler.test.js
+++ tests/activityHandler.test.js
@@ -14,6 +14,7 @@ describe('removeEntry', function() {
     it('should remove node if it is unliked and remove.unliked is true', function(done) {
         fs.readFile('./tests/data/activity-unliked.html', 'utf8', function(err, htmlContent) {
             if (err) throw err;
+            config.remove.uncommented = false;
             config.remove.unliked = true;
             const dom = new jsdom.JSDOM(htmlContent);
             const node = dom.window.document.body.firstChild;
@@ -25,6 +26,54 @@ describe('removeEntry', function() {
             done();
         });
     });
+
+    it('should remove node if it is uncommented and remove.uncommented is true', function(done) {
+        fs.readFile('./tests/data/activity-uncommented.html', 'utf8', function(err, htmlContent) {
+            if (err) throw err;
+            config.remove.uncommented = true;
+            const dom = new jsdom.JSDOM(htmlContent);
+            const node = dom.window.document.body.firstChild;
+            const removeSpy = spy(node, 'remove');
+            activityHandler.removeEntry(node);
+
+            expect(removeSpy.calledOnce).to.be.true;
+            expect(activityHandler.currentLoadCount).to.equal(0);
+            done();
+        });
+    });
+
+    it('should remove node if it contains images and remove.images is true', function(done) {
+        fs.readFile('./tests/data/activity-with-images.html', 'utf8', function(err, htmlContent) {
+            if (err) throw err;
+            config.remove.uncommented = false;
+            config.remove.images = true;
+            const dom = new jsdom.JSDOM(htmlContent);
+            const node = dom.window.document.body.firstChild;
+            const removeSpy = spy(node, 'remove');
+            activityHandler.removeEntry(node);
+
+            expect(removeSpy.calledOnce).to.be.true;
+            expect(activityHandler.currentLoadCount).to.equal(0);
+            done();
+        });
+    });
+
+    it('should remove node if it contains videos and remove.videos is true', function(done) {
+        fs.readFile('./tests/data/activity-with-videos.html', 'utf8', function(err, htmlContent) {
+            if (err) throw err;
+            config.remove.uncommented = false;
+            config.remove.videos = true;
+            const dom = new jsdom.JSDOM(htmlContent);
+            const node = dom.window.document.body.firstChild;
+            const removeSpy = spy(node, 'remove');
+            activityHandler.removeEntry(node);
+
+            expect(removeSpy.calledOnce).to.be.true;
+            expect(activityHandler.currentLoadCount).to.equal(0);
+            done();
+        });
+    });
+
 
     afterEach(function() {
         activityHandler.resetState();
