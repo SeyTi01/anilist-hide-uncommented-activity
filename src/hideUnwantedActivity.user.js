e46c53e112361d16c3b6b7d2456ae49f5ebcdea3
SeyTi01 <jannis.ridder@gmail.com>
2023-10-16 17:56:12 +0200

    Revert "Revert "[REFACTOR] arrow functions""
    This reverts commit ec9c9969935998cf8da7fa243d5870ec2cbef452.

--- src/hideUnwantedActivity.user.js
+++ src/hideUnwantedActivity.user.js
@@ -169,49 +169,47 @@ class ActivityHandler {
 
 class UIHandler {
 
-    constructor() {
-        this.userPressed = true;
-        this.cancel = null;
-        this.loadMore = null;
-    }
+    userPressed = true;
+    cancel = null;
+    loadMore = null;
 
-    setLoadMore(button) {
+    setLoadMore = (button) => {
         this.loadMore = button;
         this.loadMore.addEventListener('click', () => {
             this.userPressed = true;
             this.simulateDomEvents();
             this.showCancel();
         });
-    }
+    };
 
-    clickLoadMore() {
+    clickLoadMore = () => {
         if (this.loadMore) {
             this.loadMore.click();
             this.loadMore = null;
         }
-    }
+    };
 
-    resetState() {
+    resetState = () => {
         this.userPressed = false;
         this.hideCancel();
-    }
+    };
 
-    showCancel() {
+    showCancel = () => {
         if (!this.cancel) {
             this.createCancel();
         } else {
             this.cancel.style.display = 'block';
         }
-    }
+    };
 
-    hideCancel() {
+    hideCancel = () => {
         if (this.cancel) {
             this.cancel.style.display = 'none';
         }
-    }
+    };
 
-    simulateDomEvents() {
-        const domEvent = new Event('scroll', {bubbles: true});
+    simulateDomEvents = () => {
+        const domEvent = new Event('scroll', { bubbles: true });
         const intervalId = setInterval(() => {
             if (this.userPressed) {
                 window.dispatchEvent(domEvent);
@@ -219,9 +217,9 @@ class UIHandler {
                 clearInterval(intervalId);
             }
         }, 100);
-    }
+    };
 
-    createCancel() {
+    createCancel = () => {
         const BUTTON_STYLE = `
             position: fixed;
             bottom: 10px;
@@ -247,7 +245,7 @@ class UIHandler {
         });
 
         document.body.appendChild(this.cancel);
-    }
+    };
 }
 
 class ConfigValidator {
