1ffb9e8a88cd0f83ec499e2bc588e0507026aaa7
SeyTi01 <jannis.ridder@gmail.com>
2023-10-18 16:48:57 +0200

    [REFACTOR] extracted defaultConfig

--- tests/configValidator.test.js
+++ tests/configValidator.test.js
@@ -1,5 +1,6 @@
 const { expect } = require('chai');
 const { ConfigValidator } = require('../src/hideUnwantedActivity.user');
+const merge = require('lodash.merge');
 
 describe('ConfigValidator', () => {
     const defaultConfig = {
@@ -24,28 +25,20 @@ describe('ConfigValidator', () => {
         },
     };
 
+    function generateMergedConfig(configOverrides) {
+        return merge({}, defaultConfig, configOverrides);
+    }
+
     it('should validate a valid configuration without errors', () => {
-        const validConfig = {
+        const validConfig = generateMergedConfig({
             remove: {
-                uncommented: false,
-                unliked: false,
-                text: false,
-                images: false,
-                videos: false,
                 containsStrings: ['A'],
-                notContainsStrings: [['B']],
+                notContainsStrings: [['A']],
             },
             options: {
-                targetLoadCount: 10,
-                caseSensitive: false,
                 linkedConditions: ['text'],
             },
-            runOn: {
-                home: false,
-                social: false,
-                profile: false,
-            },
-        };
+        });
 
         const validator = new ConfigValidator(validConfig);
         expect(() => validator.validate()).to.not.throw();
@@ -53,27 +46,13 @@ describe('ConfigValidator', () => {
     });
 
     it('should validate boolean keys correctly', () => {
-        const invalidConfig = {
+        const invalidConfig = generateMergedConfig({
             remove: {
                 uncommented: 'invalid',
                 unliked: 123,
                 text: 'invalid',
-                images: false,
-                videos: false,
-                containsStrings: ['A'],
-                notContainsStrings: [['B']],
             },
-            options: {
-                targetLoadCount: 10,
-                caseSensitive: false,
-                linkedConditions: ['text'],
-            },
-            runOn: {
-                home: false,
-                social: false,
-                profile: false,
-            },
-        };
+        });
 
         const validator = new ConfigValidator(invalidConfig);
         expect(() => validator.validate()).to.throw(/should be a boolean/);
@@ -81,27 +60,11 @@ describe('ConfigValidator', () => {
     });
 
     it('should validate positive non-zero integers correctly', () => {
-        const invalidConfig = {
-            remove: {
-                uncommented: false,
-                unliked: false,
-                text: false,
-                images: false,
-                videos: false,
-                containsStrings: [],
-                notContainsStrings: [],
-            },
+        const invalidConfig = generateMergedConfig({
             options: {
                 targetLoadCount: 0,
-                caseSensitive: false,
-                linkedConditions: [],
             },
-            runOn: {
-                home: false,
-                social: false,
-                profile: false,
-            },
-        };
+        });
 
         const validator = new ConfigValidator(invalidConfig);
         expect(() => validator.validate()).to.throw(/should be a positive non-zero integer/);
@@ -109,25 +72,22 @@ describe('ConfigValidator', () => {
     });
 
     it('should validate array keys correctly', () => {
-        const invalidConfig = Object.assign({}, defaultConfig, {
+        const invalidConfig = generateMergedConfig({
             remove: {
-                uncommented: false,
-                unliked: false,
-                text: false,
-                images: false,
-                videos: false,
                 containsStrings: 'invalid',
                 notContainsStrings: 'invalid',
             },
-            options: {
-                targetLoadCount: 10,
-                caseSensitive: false,
-                linkedConditions: [],
-            },
-            runOn: {
-                home: false,
-                social: false,
-                profile: false,
+        });
+
+        const validator = new ConfigValidator(invalidConfig);
+        expect(() => validator.validate()).to.throw(/should be an array/);
+        expect(validator.errors).to.be.an('array').that.is.not.empty;
+    });
+
+    it('should validate array keys with non-array values correctly', () => {
+        const invalidConfig = generateMergedConfig({
+            remove: {
+                notContainsStrings: true,
             },
         });
 
@@ -137,53 +97,9 @@ describe('ConfigValidator', () => {
     });
 
     it('should validate linked conditions with strings correctly', () => {
-        const invalidConfig = Object.assign({}, defaultConfig, {
-            remove: {
-                uncommented: false,
-                unliked: false,
-                text: false,
-                images: false,
-                videos: false,
-                containsStrings: [],
-                notContainsStrings: [],
-            },
+        const invalidConfig = generateMergedConfig({
             options: {
-                targetLoadCount: 10,
-                caseSensitive: false,
                 linkedConditions: ['invalid', 'unliked'],
-            },
-            runOn: {
-                home: false,
-                social: false,
-                profile: false,
-            },
-        });
-
-        const validator = new ConfigValidator(invalidConfig);
-        expect(() => validator.validate()).to.throw(/should only contain the following strings/);
-        expect(validator.errors).to.be.an('array').that.is.not.empty;
-    });
-
-    it('should validate linked conditions inner array with strings correctly', () => {
-        const invalidConfig = Object.assign({}, defaultConfig, {
-            remove: {
-                uncommented: false,
-                unliked: false,
-                text: false,
-                images: false,
-                videos: false,
-                containsStrings: [],
-                notContainsStrings: [],
-            },
-            options: {
-                targetLoadCount: 10,
-                caseSensitive: false,
-                linkedConditions: [['invalid', 'unliked']],
-            },
-            runOn: {
-                home: false,
-                social: false,
-                profile: false,
             },
         });
 
@@ -193,53 +109,9 @@ describe('ConfigValidator', () => {
     });
 
     it('should validate linked conditions array with boolean correctly', () => {
-        const invalidConfig = Object.assign({}, defaultConfig, {
-            remove: {
-                uncommented: false,
-                unliked: false,
-                text: false,
-                images: false,
-                videos: false,
-                containsStrings: [],
-                notContainsStrings: [],
-            },
+        const invalidConfig = generateMergedConfig({
             options: {
-                targetLoadCount: 10,
-                caseSensitive: false,
                 linkedConditions: [true],
-            },
-            runOn: {
-                home: false,
-                social: false,
-                profile: false,
-            },
-        });
-
-        const validator = new ConfigValidator(invalidConfig);
-        expect(() => validator.validate()).to.throw(/should only contain the following strings/);
-        expect(validator.errors).to.be.an('array').that.is.not.empty;
-    });
-
-    it('should validate linked conditions inner array with boolean correctly', () => {
-        const invalidConfig = Object.assign({}, defaultConfig, {
-            remove: {
-                uncommented: false,
-                unliked: false,
-                text: false,
-                images: false,
-                videos: false,
-                containsStrings: [],
-                notContainsStrings: [],
-            },
-            options: {
-                targetLoadCount: 10,
-                caseSensitive: false,
-                linkedConditions: [[true]],
-            },
-            runOn: {
-                home: false,
-                social: false,
-                profile: false,
             },
         });
 
@@ -249,25 +121,10 @@ describe('ConfigValidator', () => {
     });
 
     it('should validate string based removal correctly', () => {
-        const invalidConfig = Object.assign({}, defaultConfig, {
+        const invalidConfig = generateMergedConfig({
             remove: {
-                uncommented: false,
-                unliked: false,
-                text: false,
-                images: false,
-                videos: false,
                 containsStrings: [true],
                 notContainsStrings: [true],
-            },
-            options: {
-                targetLoadCount: 10,
-                caseSensitive: false,
-                linkedConditions: [],
-            },
-            runOn: {
-                home: false,
-                social: false,
-                profile: false,
             },
         });
 
@@ -277,25 +134,10 @@ describe('ConfigValidator', () => {
     });
 
     it('should validate string based removal with inner arrays correctly', () => {
-        const invalidConfig = Object.assign({}, defaultConfig, {
+        const invalidConfig = generateMergedConfig({
             remove: {
-                uncommented: false,
-                unliked: false,
-                text: false,
-                images: false,
-                videos: false,
                 containsStrings: [[true]],
                 notContainsStrings: [[true]],
-            },
-            options: {
-                targetLoadCount: 10,
-                caseSensitive: false,
-                linkedConditions: [],
-            },
-            runOn: {
-                home: false,
-                social: false,
-                profile: false,
             },
         });
 
