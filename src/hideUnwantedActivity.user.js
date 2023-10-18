c2606887bbd034cc435bada57a8147ff1bf60a70
SeyTi01 <jannis.ridder@gmail.com>
2023-10-18 17:14:18 +0200

    [REFACTOR] removed config export

--- src/hideUnwantedActivity.user.js
+++ src/hideUnwantedActivity.user.js
@@ -459,4 +459,4 @@ if (require.main === module) {
     main();
 }
 
-module.exports = { MainApp, ActivityHandler, UIHandler, ConfigValidator, config, SELECTORS };
+module.exports = { MainApp, ActivityHandler, UIHandler, ConfigValidator, SELECTORS };
