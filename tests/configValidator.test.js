e2b6bb91863f5ea3cc5fc88c9a6dfdd6d2406e78
SeyTi01 <jannis.ridder@gmail.com>
2023-10-19 22:04:51 +0200

    [REFACTOR]

--- tests/configValidator.test.js
+++ tests/configValidator.test.js
@@ -11,12 +11,12 @@ describe('ConfigValidator', () => {
             images: false,
             videos: false,
             containsStrings: [],
-            notContainsStrings: [],
         },
         options: {
             targetLoadCount: 10,
             caseSensitive: false,
             linkedConditions: [],
+            reversedConditions: false,
         },
         runOn: {
             home: false,
@@ -28,100 +28,53 @@ describe('ConfigValidator', () => {
     const generateMergedConfig = (configOverrides) => merge({}, defaultConfig, configOverrides);
 
     const validateAndCheckErrors = (config, errorMessage) => {
-        const validator = new ConfigValidator(config);
-        const errorsBeforeValidation = validator.errors.length;
-        expect(() => validator.validate()).to.throw(errorMessage);
-        expect(validator.errors).to.have.length.at.least(errorsBeforeValidation);
+        it(`should validate the configuration and throw error: ${errorMessage}`, () => {
+            const validator = new ConfigValidator(config);
+            expect(() => validator.validate()).to.throw(errorMessage);
+        });
     };
 
-    it('should validate a valid configuration without errors', () => {
-        const validConfig = generateMergedConfig({
-            remove: {
-                containsStrings: ['A'],
-                notContainsStrings: [['A']],
-            },
-            options: {
-                linkedConditions: ['text'],
-            },
-        });
-        const validator = new ConfigValidator(validConfig);
-        expect(() => validator.validate()).to.not.throw();
+    const validConfig = generateMergedConfig({
+        remove: {
+            containsStrings: ['A'],
+            notContainsStrings: [['A']],
+        },
+        options: {
+            linkedConditions: ['text'],
+        },
     });
 
-    it('should validate boolean keys correctly', () => {
-        const invalidConfig = generateMergedConfig({
-            remove: {
-                uncommented: 'invalid',
-                unliked: 123,
-                text: 'invalid',
-            },
-        });
-        validateAndCheckErrors(invalidConfig, /should be a boolean/);
-    });
+    const testCases = [
+        { config: validConfig, errorMessage: null }, // A valid config with no error
 
-    it('should validate positive non-zero integers correctly', () => {
-        const invalidConfig = generateMergedConfig({
-            options: {
-                targetLoadCount: 0,
-            },
-        });
-        validateAndCheckErrors(invalidConfig, /should be a positive non-zero integer/);
-    });
+        {
+            config: generateMergedConfig({
+                remove: {
+                    uncommented: 'invalid',
+                    unliked: 123,
+                    text: 'invalid',
+                },
+            }),
+            errorMessage: /should be a boolean/,
+        },
+        {
+            config: generateMergedConfig({
+                options: {
+                    targetLoadCount: 0,
+                },
+            }),
+            errorMessage: /should be a positive non-zero integer/,
+        },
+    ];
 
-    it('should validate array keys correctly', () => {
-        const invalidConfig = generateMergedConfig({
-            remove: {
-                containsStrings: 'invalid',
-                notContainsStrings: 'invalid',
-            },
-        });
-        validateAndCheckErrors(invalidConfig, /should be an array/);
-    });
-
-    it('should validate array keys with non-array values correctly', () => {
-        const invalidConfig = generateMergedConfig({
-            remove: {
-                containsStrings: true,
-            },
-        });
-        validateAndCheckErrors(invalidConfig, /should be an array/);
-    });
-
-    it('should validate linked conditions with strings correctly', () => {
-        const invalidConfig = generateMergedConfig({
-            options: {
-                linkedConditions: ['invalid', 'unliked'],
-            },
-        });
-        validateAndCheckErrors(invalidConfig, /should only contain the following strings/);
-    });
-
-    it('should validate linked conditions array with boolean correctly', () => {
-        const invalidConfig = generateMergedConfig({
-            options: {
-                linkedConditions: [true],
-            },
-        });
-        validateAndCheckErrors(invalidConfig, /should only contain the following strings/);
-    });
-
-    it('should validate string-based removal correctly', () => {
-        const invalidConfig = generateMergedConfig({
-            remove: {
-                containsStrings: [true],
-                notContainsStrings: [true],
-            },
-        });
-        validateAndCheckErrors(invalidConfig, /should only contain strings/);
-    });
-
-    it('should validate string-based removal with inner arrays correctly', () => {
-        const invalidConfig = generateMergedConfig({
-            remove: {
-                containsStrings: [[true]],
-                notContainsStrings: [[true]],
-            },
-        });
-        validateAndCheckErrors(invalidConfig, /should only contain strings/);
+    testCases.forEach(({ config, errorMessage }) => {
+        if (errorMessage) {
+            validateAndCheckErrors(config, errorMessage);
+        } else {
+            it('should validate a valid configuration without errors', () => {
+                const validator = new ConfigValidator(config);
+                expect(() => validator.validate()).to.not.throw();
+            });
+        }
     });
 });
