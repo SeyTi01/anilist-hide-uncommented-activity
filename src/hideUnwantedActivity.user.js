e2e7b6da1c4d29c8a02d1153aae46e879c475a0c
SeyTi01 <jannis.ridder@gmail.com>
2023-11-03 12:36:50 +0100

    [REFACTOR] shouldRemoveNode

--- src/hideUnwantedActivity.user.js
+++ src/hideUnwantedActivity.user.js
@@ -119,13 +119,9 @@ class ActivityHandler {
 
     shouldRemoveNode = (node) => {
         const { remove, options: { linkedConditions, reversedConditions } } = this.config;
-
         const shouldSkipChecking = (condition) => !!(linkedConditions && linkedConditions.flat().includes(condition));
-
         const evaluateCondition = ([name, predicate]) => !shouldSkipChecking(name) && remove[name] && predicate(node, reversedConditions);
-
         const conditionEntries = Array.from(this.conditionsMap.entries());
-
         let shouldRemoveByCondition;
 
         if (reversedConditions && Object.values(remove).filter(Boolean).length >= 2) {
